b7452c4c2d589a13a17710b700c391fd2972ab39
Neonkotick <libraryofgames@mail.ru>
2026-09-11 14:16:28 +0300

    build: disable sourcemaps for lighter production builds

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,21 @@ import { resolve } from 'path';
 
 export default defineConfig({
   base: './',
-  resolve: { alias: { '@': resolve(__dirname, 'src') } },
-  build: { target: 'es2020', outDir: 'dist', assetsDir: 'assets', sourcemap: true },
-  server: { host: true, port: 5173 },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    target: 'es2020',
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 2000,
+  },
+  server: {
+    host: true,
+    port: 5173,
+  },
 });
